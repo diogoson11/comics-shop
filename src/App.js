b6ee0474fba070cc3362c10a182496e696801863
Diogo <diogofoguedes@hotmail.com>
2021-10-29 19:08:18 +0100

    update menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import {Route,Switch} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+
+
+const ComicBooksPage = () => (
+  <div>
+    <h1>COMIC BOOKS Page</h1>
+
+  </div>
+
+
+)
 
 
 function App() {
   return (
     <div>
-    <Homepage/>
+    <Switch>
+    <Route exact  path='/' component={Homepage}/>
+    <Route   path='/comicbooks' component={ComicBooksPage}/>
+    </Switch>
     </div>
      
     
