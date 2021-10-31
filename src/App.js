ebfe173229c70866f66e91206539adcdc82a0454
Diogo <diogofoguedes@hotmail.com>
2021-10-31 18:54:06 +0000

    Sig in Form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import {Route,Switch} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
+import SignInAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 
 
@@ -10,9 +11,11 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
     <Route exact  path='/' component={Homepage}/>
     <Route path='/shop' component={ShopPage}/>
+    <Route path='/signin' component={SignInAndSignOut}/>
     </Switch>
     </div>
      
